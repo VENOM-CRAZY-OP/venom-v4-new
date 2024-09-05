01e1f493f5e8c06a54b104e206f1d217984ad024
VENOM-CRAZY-OP <cyberecurityind@gmail.com>
2024-09-05 15:22:59 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY VENOMxCRAZY
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("TOKEN")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("venomXcrazy")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5588464519")
+
+
+
+
+
+
+
+  
