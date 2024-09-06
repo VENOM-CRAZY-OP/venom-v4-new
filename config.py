48fd2fba74e3a5fb0d845d5fa0f0b8b577bbb6ef
SRxCA <jyotirbedant@gmail.com>
2024-09-06 17:52:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7201688855:AAECH5eKVzpAdedrnosC3DZxeTgHaTUxR84")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("sibuna")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7186024164")
 
 
 
